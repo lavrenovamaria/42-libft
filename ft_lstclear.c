301613bfe513ecc9f738d98ba40bca8f1452fb2d
lavrenovamaria <wrickard@student.21-school.ru>
2021-08-26 17:08:46 +0300

    ft_lstclear

---
+++ ft_lstclear.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wrickard <wrickard@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/18 20:14:30 by wrickard          #+#    #+#             */
+/*   Updated: 2021/08/18 20:18:06 by wrickard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	t_list *list;
+
+	if (!del)
+		return ;
+	while (*lst)
+	{
+		del((*lst)->content);
+		list = *lst;
+		*lst = list->next;
+		free(list);
+	}
+	*lst = NULL;
+}
